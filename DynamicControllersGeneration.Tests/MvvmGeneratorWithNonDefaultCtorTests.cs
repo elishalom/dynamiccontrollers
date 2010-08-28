03b6ff7198698e16fd0b2ba3fb1f004a144881ed
Elisha <Elisha@localhost>
2010-08-28 14:36:44 +0300

    Added ability to pass model through constructor

---
+++ DynamicControllersGeneration.Tests/MvvmGeneratorWithNonDefaultCtorTests.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+
+namespace DynamicControllersGeneration.Tests
+{
+    [TestFixture]
+    public class MvvmGeneratorWithNonDefaultCtorTests
+    {
+        public class Model
+        {
+            public object Prop { get; set; }
+        }
+
+        public abstract class ViewModel : INotifyPropertyChanged
+        {
+            public Model Model { get; private set; }
+
+            public abstract object Prop { get; set; }
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected ViewModel(Model model)
+            {
+                Model = model;
+            }
+        }
+
+        [Test]
+        public void Generate_ViewModelExpectingModelInCtor_RecieveModelInCtor()
+        {
+            var viewModelGenerator = new ViewModelGenerator();
+
+            var model = new Model();
+            ViewModel viewModel = viewModelGenerator.Generate<ViewModel>(model);
+
+            Assert.That(viewModel.Model, Is.SameAs(model));
+        }
+    }
+}
--- DynamicControllersGeneration/ViewModelGenerator.cs
+++ DynamicControllersGeneration/ViewModelGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Castle.DynamicProxy;
+﻿using System.Linq;
+using System.Reflection;
+using Castle.DynamicProxy;
 
 namespace DynamicControllersGeneration
 {
@@ -18,10 +20,39 @@ namespace DynamicControllersGeneration
             var proxyGenerationOptions = new ProxyGenerationOptions(propertiesFilter);
             var modelInterceptor = new ModelInterceptor(model);
 
-            var classProxy = proxyGenerator.CreateClassProxy<T>(proxyGenerationOptions, modelInterceptor);
+            object[] constructorArguments = GetConstructorArguments<T>(model);
+            var classProxy = (T) proxyGenerator.CreateClassProxy(typeof (T), proxyGenerationOptions, constructorArguments, modelInterceptor);
             modelInterceptor.RegisterProxy(classProxy);
 
             return classProxy;
+        }
+
+        private static object[] GetConstructorArguments<T>(object model)
+        {
+            var nonPrivateConstructors = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .Where(constructor => !constructor.IsPrivate);
+            var hasConstructorExpectingModel = nonPrivateConstructors
+                .Any(constructor => IsContructorExpectingModel(constructor, model));
+
+            if (hasConstructorExpectingModel)
+            {
+                return new[] {model};
+            }
+            
+            return new object[0];
+        }
+
+        private static bool IsContructorExpectingModel(ConstructorInfo constructor, object model)
+        {
+            var constructorParameters = constructor.GetParameters();
+            var isExpectingSingleArgument = constructorParameters.Length == 1;
+            if (!isExpectingSingleArgument)
+            {
+                return false;
+            }
+
+            var isParametersMatchingModel = constructorParameters[0].ParameterType.IsAssignableFrom(model.GetType());
+            return isParametersMatchingModel;
         }
     }
 }
